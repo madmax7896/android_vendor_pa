9df1282e99840bac9f86b6508bdc1068824d3c05
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-11 17:48:55 +0530

    properties: Drop redfin build fingerprint
     * we'll spoof fingerprint gms in fw/b instead
    
    Change-Id: I5e9ba4bf941863d9331da9fb606398e2a7bbe64c

--- config/properties.mk
+++ config/properties.mk
@@ -35,11 +35,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
-# Fingerprint
-ifeq ($(TARGET_BUILD_VARIANT),user)
-BUILD_FINGERPRINT ?= google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-endif
-
 # Gestures
 ifneq ($(TARGET_USES_HARDWARE_KEYS),true)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
